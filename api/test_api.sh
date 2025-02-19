a886bc8db9a257425efbca1991253da0b950af6c
root <839354647@qq.com>
2025-02-19 07:41:01 +0000

    add nginx example

--- api/test_api.sh
+++ api/test_api.sh
@@ -2,4 +2,4 @@ curl -X POST "http://0.0.0.0:7870/generate-soundful-video" \
      -H "Content-Type: application/json" \
      -d '{"prompt": "A tiger walking in snow", "aspect_ratio": "1:1"}'
 
-curl -X GET "http://0.0.0.0:7870/task/7a6e4b03-9a7c-42bd-a3bc-e08fc5e0c294"
+curl -X GET "http://0.0.0.0:7870/task/e4fbc69a-bdb7-4d7a-bab9-9c9f308db6cd"
