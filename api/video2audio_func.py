17a5153f4f26bffa54f2e7cdc6a384b0a7432622
root <839354647@qq.com>
2025-03-06 08:23:26 +0000

    fixed soundful video gen

--- api/video2audio_func.py
+++ api/video2audio_func.py
@@ -4,19 +4,22 @@ from gradio_client import Client
 from api.utils.url_utils import remove_storage_prefix, add_storage_prefix
 from api.video_transport import download_blob, upload_blob
 
-def video2audio(video_path, user_id, task_id, prompt, negative_prompt):
+def video2audio(video_path, user_id, task_id, prompt="", negative_prompt="music"):
 	client = Client("http://0.0.0.0:7860/")
 	print(f"生成有声视频: {video_path}")
 
-	# 移除storage前缀
-	source_blob_name = remove_storage_prefix(video_path)
-	print(f"开始下载视频文件: {source_blob_name}")
-	local_file_path = download_blob(
-		bucket_name="soundful",
-		source_blob_name=source_blob_name,
-		destination_file_name=f"/workspace/tmp/{user_id}/{os.path.basename(source_blob_name)}"
-	)
-	print(f"下载视频文件完成: {local_file_path}")
+	if video_path.startswith("https://"):
+		# 移除storage前缀
+		source_blob_name = remove_storage_prefix(video_path)
+		print(f"开始下载视频文件: {source_blob_name}")
+		local_file_path = download_blob(
+			bucket_name="soundful",
+			source_blob_name=source_blob_name,
+			destination_file_name=f"/workspace/tmp/{user_id}/{os.path.basename(source_blob_name)}"
+		)
+		print(f"下载视频文件完成: {local_file_path}")
+	else:
+		local_file_path = video_path
 
 	result = client.predict(
 			video=local_file_path,
@@ -40,6 +43,6 @@ def video2audio(video_path, user_id, task_id, prompt, negative_prompt):
 	)
 
 	final_url = add_storage_prefix(upload_result)
-	print(f"上传音频文件到GCS完成: {final_url}")
+	print(f"上传有声视频文件到GCS完成: {final_url}")
 
 	return final_url
